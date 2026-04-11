2d86875cfaabac2d0e23839daf995fd7a65fedac
abubakar83 <abubakar.abs83@gmail.com>
2026-04-11 14:52:48 +0500

    adding questions

--- script.js
+++ script.js
@@ -12,4 +12,56 @@ const finalScoreSpan = document.getElementById("final-score")
 const maxScoreSpan = document.getElementById('max-score')
 const resultMessage = document.getElementById('result-message')
 const restartButton = document.getElementById("restart-btn")
-const progressBar = document.getElementById('progress')
+const progressBar = document.getElementById('progress')\
+
+// Quiz-Question 
+const quizQuestions =  [
+    {
+        question: "What is the capital of France ?",
+        answer: [
+            {
+                text: "London", correct: false,
+                text: "Berlin", correct: false,
+                text: "Paris", correct: true,
+                text:  "Madrid", correct: false,
+            }
+        ],
+    
+    },
+    {
+        question: "Whic planet is known as Red Planet ?",
+        answer: [
+            { text: "Venus", correct: false},
+            { text: "Mars", corrent: true},
+            { text: "Jupter", correct: false},
+            { text: "Satrun", correct: false},
+        ],
+    },
+    {
+        question: "What is largest ocean on Earth ?",
+        answer: [
+            { text: 'Atlantic Ocean', correct: false},
+            { text: "Indian Ocean", correct: false},
+            { text: "Arctic Ocean", correct: false},
+            { text: "Pacific Ocean", correct: true}
+        ],
+    }, 
+    {
+        question: "Which of these in NOT a programming language ?",
+        answer: [
+            { text: 'Java', correct: false },
+            { text: 'Python', correct: false },
+            { text: 'Banana', correct: true},
+            { text: 'JavaScript', correct: false},
+        ],
+    },
+    {
+        question: "What is the chemical symbol for gold ?",
+        answer: [
+            { text: "Go", correct: false },
+            { text: "Gd", correct: false },
+            { text: 'Au', correct: true },
+            { text: "Ag", correct: false }, 
+        ],
+    },
+]
